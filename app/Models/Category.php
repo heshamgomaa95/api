bbbff1cc5c5fd7be76c6e18b6d8db86517ea1cf2
Your Name <you@example.com>
2020-08-02 19:46:40 +0200

    first commit

---
+++ app/Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table='categories';
+    protected $fillable=['name_ar','name_en','created_at','updated_at'];
+    protected $hidden=['created_at','updated_at'];
+    public $timestamps=false;
+}
